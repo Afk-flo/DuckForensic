821f43f4d774040708f7d3f4a668171862907b5a
afk-flo <florian.chauvain@gmail.com>
2025-11-30 14:06:22 +0100

    More user

--- src/user.py
+++ src/user.py
@@ -1,4 +1,8 @@
+import os
+
 import psutil
+from pathlib import Path
+import pwd
 
 def get_user():
     """
@@ -10,5 +14,15 @@ def get_user():
     for user in users:
         username = user.name
         print("[+] Username: {}".format(username))
+        userI = pwd.getpwnam(username)
+        print("--> UID: {}".format(userI.pw_uid))
+        print("--> GID: {}".format(userI.pw_gid))
+        print("--> DIR: {}".format(userI.pw_dir))
+        print("--> SHELL: {}".format(userI.pw_shell))
+
+
+        # Check for /home except root
+        if not Path(f"/home/{username}").exists():
+            print("[!] User {} don't have a Home - Is this user legit? [!]".format(username))
 
 
